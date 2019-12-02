97b4d307370ed9865133aaf9fc9e9a0e09f65784
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-12-02 23:48:47 +0600

    Navbar done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Navbar extends Component {
     state = {  }
     render() { 
         return (
-            <div>
-                <h3>Hello From Navbar</h3>
-            </div>
+            <nav className="navbar navbarWrapper navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to="/" className="logo-top">
+                    <h1>Kisko</h1>
+                </Link>
+                <ul className="navbar-nav align-item-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">Products</Link>
+                    </li>
+                </ul>
+
+                <Link to="/cart" className="ml-auto myCartBtn">
+                    <button><FontAwesomeIcon icon={['fas', 'cart-plus']} />
+                        My Carts
+                    </button>
+                </Link>
+            </nav>
         );
     }
 }
