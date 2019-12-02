1739833cbb81f6b6cfa9e42596552cf535e83c04
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-12-02 23:48:28 +0600

    Remove FontAwesome icon from App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ProductList from './components/productList/ProductList'
 import Details from './components/Details/Details'
 import Cart from './components/cart/Cart'
 import Default from './components/default/Default'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
