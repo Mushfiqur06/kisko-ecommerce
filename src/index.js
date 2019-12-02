04b321054ff7356f6cb805b9552166bb902032ba
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-12-02 23:49:44 +0600

    Added CartPlus icon

--- src/index.js
+++ src/index.js
@@ -10,10 +10,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { 
     fab,
 } from '@fortawesome/free-brands-svg-icons'
-import { } from '@fortawesome/free-solid-svg-icons'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import { } from '@fortawesome/free-regular-svg-icons'
 library.add(
     fab, 
+    faCartPlus
 )
 
 
