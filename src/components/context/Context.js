67460f9f32c898d5a485e3a9bde5b26b3edf588e
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-01-03 12:27:04 +0600

    Create Product Context

---
+++ src/components/context/Context.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { storeProducts, detailProduct } from './../../data'
+
+const ProductContext = React.createContext()
+
+class ProductProvider extends Component {
+    state = {
+        products : storeProducts,
+        detailsProduct: detailProduct
+    }
+    handleDetail = () => {
+        console.log('hello from details')
+    }
+
+    addToCart = () => {
+        console.log('hello from Cart')
+    }
+    render() {
+        return (
+            <ProductContext.Provider value={{
+                products: this.state.products,
+                detailsProduct: this.state.detailsProduct,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart
+            }}>
+                {this.props.children}
+            </ProductContext.Provider>
+        )
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProductProvider, ProductConsumer}
