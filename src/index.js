3a4a6a78c1591907491ce25b892b9e096db774de
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-01-03 12:28:33 +0600

    change data.js file

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {ProductProvider} from './components/context/Context'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-import { 
-    fab,
-} from '@fortawesome/free-brands-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import { } from '@fortawesome/free-regular-svg-icons'
+
 library.add(
     fab, 
     faCartPlus
@@ -19,11 +19,16 @@ library.add(
 
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, document.getElementById('root'));
+    <ProductProvider>
+        <Router>
+            <App />
+        </Router>
+    </ProductProvider>,
+    
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
