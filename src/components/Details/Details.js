369726c71958069d24b317c42e306a18902b7931
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-01-06 23:58:16 +0600

    product details done

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import { ProductConsumer } from './../context/Context'
+import { Link } from 'react-router-dom'
 
 class Details extends Component {
     state = {  }
     render() { 
         return (
-            <div>
-                <h3>Hello From Details</h3>
-            </div>
+            <ProductConsumer>
+                {(value => {
+                    const {id, company, img, info, price, title, inCart} = value.detailsProduct;
+
+                    return (
+                        <div className="container py-5">
+                            <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                <h1>{title}</h1>
+                            </div>
+                            <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3">
+                                    <img src={img} className="img-fluid" alt="Product" />
+                                </div>
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                    <h2>Model {title}</h2>
+                                    <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                                        Made by: <span className="text-uppercase">{company}</span>
+                                    </h4>
+                                    <h4 className="text-blue">
+                                        <strong>price: <span>$</span>{price}</strong>
+                                    </h4>
+                                    <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                                        some info for product
+                                    </p>
+                                    <p className="text-muted lead">{info}</p>
+
+                                    <div>
+                                        <Link to="/">
+                                            <button className="text-capitalize p-2 mr-2" >Back to Product</button>
+                                        </Link>
+                                        <button className="text-capitalize p-2">{inCart ? 'InCart' : 'add to cart'}</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </ProductConsumer>
         );
     }
 }
